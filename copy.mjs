d513d0e5ab2ff18a73c8ec492e5dc072fd754e7e
tianheg <yidajiabei@163.com>
2022-04-12 22:34:52 +0800

    feat: life + tech

---
+++ copy.mjs
@@ -0,0 +1,16 @@
+import path from 'path'
+import fs from 'fs-extra'
+
+async function copy() {
+  const srcRoot = path.resolve('src')
+  const srcFolders = await fs.readdir(srcRoot)
+  await Promise.all(
+    srcFolders.map((srcFolder) => {
+      const copyFrom = path.join(srcRoot, srcFolder, 'public')
+      const copyTo = path.resolve(srcRoot, `../dist/${srcFolder}`)
+      return fs.copy(copyFrom, copyTo)
+    })
+  )
+}
+
+copy()
