252ba30de059c66718fe1126cecf1a3ac4a1ad2b
tianheg <yidajiabei@163.com>
2022-04-16 21:03:24 +0800

    feat: add dark mode

--- src/life/static/index.js
+++ src/life/static/index.js
@@ -1,12 +1,25 @@
-document.addEventListener("click", (event) => {
-  const actionButton = event.target.closest(`[data-action]`);
+const toggle = document.querySelector('.toggle')
+const timestamp = document.getElementById('build-timestamp')
+
+document.addEventListener('click', (event) => {
+  const actionButton = event.target.closest(`[data-action]`)
 
   if (actionButton) {
-    actionButton.closest("article").classList.toggle("expanded");
+    actionButton.closest('article').classList.toggle('expanded')
   }
-});
+})
 
-const timestamp = document.getElementById("build-timestamp");
 timestamp.innerText = new Date(
-  timestamp.getAttribute("datetime")
-).toLocaleString();
+  timestamp.getAttribute('datetime')
+).toLocaleString()
+
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html')
+  if(html.classList.contains('dark')) {
+      html.classList.remove('dark')
+      e.target.innerHTML = 'Dark mode'
+  } else {
+      html.classList.add('dark')
+      e.target.innerHTML = 'Light mode'
+  }
+})
