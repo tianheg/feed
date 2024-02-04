2deff6447b0feb512b5ccf8c390d6acee4cb3750
tianheg <i@tianheg.co>
2024-02-04 23:09:04 +0800

    Update *.csv,*.json

--- genjson.js
+++ genjson.js
@@ -4,7 +4,7 @@ const csv = require('csv-parser')
 // Read the CSV file and convert it to JSON
 const jsonData = []
 
-fs.createReadStream('feed-list.csv')
+fs.createReadStream('feeds-csv.csv')
   .pipe(csv())
   .on('data', (row) => {
     const updatedRow = {}
@@ -14,7 +14,7 @@ fs.createReadStream('feed-list.csv')
     jsonData.push(updatedRow)
   })
   .on('end', () => {
-    fs.writeFile('feed.json', JSON.stringify(jsonData, null, 2), (err) => {
+    fs.writeFile('feeds-json.json', JSON.stringify(jsonData, null, 2), (err) => {
       if (err) throw err
       console.log('CSV to JSON conversion complete')
     })
